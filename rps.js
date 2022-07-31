fbd30bb26e53eee424ef1179846f3b86e23a7c26
learnerx123 <learnerx@mail.lv>
2022-07-31 22:25:23 +0530

    Wrote playRound conditions, function tested

--- rps.js
+++ rps.js
@@ -1,23 +1,80 @@
 
 //computerChoice randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 function getComputerChoice() {
-    
-    return String()
+    //    math.random(1,3)
+    return prompt("computer");
 
 }
-
 // play return a winner of the round
-function playRound (playerSelection, computerSelection) {
-    return String("You Lose! Paper beats Rock");
+function playRound(playerSelection, computerSelection) {
+    /**/ 
+    //player wins
+    if ((playerSelection === "paper" & computerSelection === "rock")) {
+        return "playerSelection";
+    }else if (playerSelection === "rock" & computerSelection === "scissor") {
+        return "playerSelection";
+    }else if (playerSelection === "scissor" & computerSelection === "paper") {
+        return "playerSelection";
+    }
+    // computer wins
+    if ((playerSelection === "rock" & computerSelection === "paper")) {
+        return "computerSelection";
+    }else if (playerSelection === "scissor" & computerSelection === "rock") {
+        return "computerSelection";
+    }else if (playerSelection === "paper" & computerSelection === "scissor") {
+        return "computerSelection";
+    }
+    // tie
+    if ((playerSelection ===  computerSelection)) {
+        return "tie";
+    }
 }
 
-const playerSelection = prompt("enter player choice").toLowerCase();  
+// get player choice
+let playerSelection = prompt("enter player choice").toLowerCase();
+
+// get computer choice
 const computerSelection = getComputerChoice();
+
 console.log(playRound(playerSelection, computerSelection));
 
 function game() {
-    for(let i = 0; i < 5; i++) {
-        playRound();
-
+    let playerScore = 0;
+    let computerScore = 0;
+    let result ;
+   
+    for (let i = 0; i < 5; i++) {
+        result = playRound();
+        if (result === "computerSelection") {
+            computerScore++;
+        }else if (result === "playerSelection") {
+            playerScore++;
+        }
     }
+    // Winner after 5 rounds
+    let winner = playerScore > computerScore ? "player Wins"
+        : playerScore === computerScore ? " its a tie" : "computer Wins";
+
+    console.log(winner);
+
 }
+
+
+// get only allowed choice
+// const playerSelection = "";
+// function playerChoice() {
+    
+//     while (true) {
+//         if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissor") {
+//             return playerSelection;
+            
+//             break;
+//         }else {
+//         console.log("playerSelection =", playerSelection, typeof playerSelection);
+            
+//         }
+
+//     }
+
+
+// } 
