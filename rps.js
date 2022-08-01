4e653be3387bacb18a3fe220fd503131b4516914
learnerx123 <learnerx@mail.lv>
2022-08-01 16:39:35 +0530

    Check for null and empty string

--- rps.js
+++ rps.js
@@ -3,67 +3,84 @@
 function getComputerChoice() {
     //    math.random(1,3)
     let numb = Math.floor(Math.random() * (4 - 1) + 1);
-    console.log(numb) 
+    console.log(numb)
 
-    return numb === 1 ? "rock" 
-    : numb === 2 ? "paper"
-    :"scissor";
+    return numb === 1 ? "rock"
+        : numb === 2 ? "paper"
+            : "scissor";
     // return prompt("computer");
 
 }
 // play return a winner of the round
 function playRound(playerSelection, computerSelection) {
-    /**/ 
+    /**/
     //player wins
     if ((playerSelection === "paper" & computerSelection === "rock")) {
         return "playerSelection";
-    }else if (playerSelection === "rock" & computerSelection === "scissor") {
+    } else if (playerSelection === "rock" & computerSelection === "scissor") {
         return "playerSelection";
-    }else if (playerSelection === "scissor" & computerSelection === "paper") {
+    } else if (playerSelection === "scissor" & computerSelection === "paper") {
         return "playerSelection";
     }
     // computer wins
     if ((playerSelection === "rock" & computerSelection === "paper")) {
         return "computerSelection";
-    }else if (playerSelection === "scissor" & computerSelection === "rock") {
+    } else if (playerSelection === "scissor" & computerSelection === "rock") {
         return "computerSelection";
-    }else if (playerSelection === "paper" & computerSelection === "scissor") {
+    } else if (playerSelection === "paper" & computerSelection === "scissor") {
         return "computerSelection";
     }
     // tie
-    if ((playerSelection ===  computerSelection)) {
+    if ((playerSelection === computerSelection)) {
         return "tie";
     }
 }
+function askChoice() {
 
-
+}
 // get player choice
 // let playerSelection = prompt("enter player choice").toLowerCase();
 
-do {
-    var playerSelection = prompt("playerChoice");
-}while (notAllowed(playerSelection))
-// notAllowed(playerSelection)
-console.log("playerSelection: ", playerSelection)
 
-// get computer choice
-const computerSelection = getComputerChoice();
-console.log("computerSelection", computerSelection);
+
+
 
 // console.log(playRound(playerSelection, computerSelection));
 
 function game() {
     let playerScore = 0;
     let computerScore = 0;
-    let result ;
-   
-    for (let i = 0; i < 5; i++) {
+    let result;
+
+    for (let i = 1; i < 6; i++) {
+        console.log("round: ", i)
+        do {
+            var playerSelection = prompt("playerChoice");
+
+            // check for null and empty string
+            if (playerSelection === null) {
+                console.log("game cancelled");
+                return "null not allowed"
+            }else if (playerSelection === "") {
+                console.log("empty selection not allowed \n game ended");
+                return "empty string";
+            }
+            // notAllowed(playerSelection) will ask again for right input
+
+        } while (notAllowed(playerSelection))
+        console.log("playerSelection: ", playerSelection)
+
+        let computerSelection = getComputerChoice();
+        console.log("computerSelection", computerSelection);
+
+        // plays a round
         result = playRound(playerSelection, computerSelection);
         if (result === "computerSelection") {
             computerScore++;
-        }else if (result === "playerSelection") {
+        } else if (result === "playerSelection") {
             playerScore++;
         }
+
     }
     // Winner after 5 rounds
     let winner = playerScore > computerScore ? "player Wins"
@@ -74,18 +91,25 @@ function game() {
 }
 
 game()
-function notAllowed(playerSelection){
+function notAllowed(playerSelection) {
     switch (playerSelection) {
         case "rock":
             return false
-            break; 
+            break;
         case "paper":
             return false
-            break;     
+            break;
         case "scissor":
             return false
-            break; 
+            break;
+        case null:
+            alert(`null`);                
+            return false;
+            break;
+        case "":
+            return false;
+            break;
         default:
-           return true; 
+            return true;
     }
 }
